b014d47e768dc5c702ca5fd910fb492a72fb4d7f
singhprabhjot16 <137317501+singhprabhjot16@users.noreply.github.com>
2023-10-01 20:35:01 +0530

    Create Selection Sort.cpp

---
+++ Selection Sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void SelectionSort (int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min = i;
+        for (int j = i+1; j < n; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    SelectionSort(arr, n);
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+}
+
